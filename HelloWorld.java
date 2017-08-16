8116c47c79313148843fb7a34dab92290d8daad9
yaboandroid <suilinfei@sina.com>
2017-08-16 17:20:51 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("This is test demo");
+        System.out.println("this is update line");
     }
 }
